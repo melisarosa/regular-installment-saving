f298bc33e4e3ba17818b5013af0f361b6a83d0fb
Melisa Varani <m.varani@allegrodevelopment.com>
2021-09-19 00:36:52 +0700

    commit change log

---
+++ src/main/resources/change_log.sql
@@ -0,0 +1,10 @@
+create table  installment_saving(
+	id int auto_increment,
+	tenor int not null,
+	first_deposit_amount numeric(28,10) not null, 
+	monthly_deposit_amount numeric(28,10) not null, 
+	purpose varchar(200),
+	status varchar(50) not null,
+	creation_date datetime not null,
+	primary key(id)
+);
