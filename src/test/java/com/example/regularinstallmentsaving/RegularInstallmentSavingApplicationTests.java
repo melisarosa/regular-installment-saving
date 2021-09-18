38f5cb84b0e75d363a8a202809fdfff381fafa9c
Melisa Varani <m.varani@allegrodevelopment.com>
2021-09-18 11:23:21 +0700

    init project

---
+++ src/test/java/com/example/regularinstallmentsaving/RegularInstallmentSavingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.regularinstallmentsaving;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegularInstallmentSavingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
